4863e00e8450fe20359d01857dda8b6ae534f56a
Laura <lauravtd44@gmail.com>
2025-04-25 09:10:53 -0400

    Initial sharing of project

---
+++ Solution.java
@@ -0,0 +1,65 @@
+
+/**
+ * Write a description of class Solution here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Solution
+{
+
+
+    public Solution()
+    {
+
+    }
+
+    //Question 24
+    public void question24(){
+    //(a)
+    String a="yes";
+    String b = "no";
+    //(b)
+    /**if (a.equals(b)){
+        System.out.print("equal");
+    else //(c)
+     System.out.print(a+" not equal to "+b);}  */
+   
+    }
+    
+    /**question 25
+    */
+ /**   public void temperatureInfo(temperatures){
+        int C=0;
+        double M=0;
+        for (double T:temperatures){
+            if (T>37.5) {
+            C++;
+            }
+        }
+        for (double t:temperatures){
+            if (M>t){
+                M=t;
+            }
+        }
+        System.out.print(C+" has condition of fever and "+M+"was the max temp");
+    }*/
+    
+    /**question 26
+    */
+ /**   public void temperatureInfo(temperatures){
+        double M;
+        M=temperatures.max();
+        System.out.print("the max temp is "+M);
+    }*/
+    
+    /**question 27
+    */
+ /**   public void temperatureInfo(temperatures){
+        temperatures.sort();
+        double M;
+        M=temperatures(temperatures.size()-1); //index of the last value in the list
+        System.out.print("max temp: "+M);
+    }   */
+
+}
