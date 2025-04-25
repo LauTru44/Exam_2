f3a1f9f10aaedac100e3a05707f1261d6acbc351
Laura <lauravtd44@gmail.com>
2025-04-25 09:49:37 -0400

    Personal work

--- Solution.java
+++ Solution.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Write a description of class Solution here.
@@ -8,28 +10,36 @@
 public class Solution
 {
 
-
     public Solution()
     {
-
+        ArrayList<Double> temperatures = new ArrayList();
+        temperatures.add(35.6);
+        temperatures.add(41.8);
+        temperatures.add(37.6);
+        temperatures.add(34.2);
+        
+        temperatureInfo(temperatures);
     }
 
     //Question 24
     public void question24(){
+    /**comments:
+     * - Missed of braces
+     */    
     //(a)
     String a="yes";
     String b = "no";
     //(b)
-    /**if (a.equals(b)){
+        if (a.equals(b)){
         System.out.print("equal");
-    else //(c)
-     System.out.print(a+" not equal to "+b);}  */
-   
+        } else {//(c)
+         System.out.print(a+" not equal to "+b);
+        }
     }
     
     /**question 25
     */
- /**   public void temperatureInfo(temperatures){
+     public void temperatureInfo(ArrayList<Double> temperatures){
         int C=0;
         double M=0;
         for (double T:temperatures){
@@ -42,24 +52,24 @@ public class Solution
                 M=t;
             }
         }
-        System.out.print(C+" has condition of fever and "+M+"was the max temp");
-    }*/
+        System.out.print(C+" have condition of fever and "+M+" was the max temp");
+    }
     
     /**question 26
     */
- /**   public void temperatureInfo(temperatures){
+    public void temperatureInfo26(ArrayList<Double> temperatures){
         double M;
-        M=temperatures.max();
+        M= Collections.max(temperatures);
         System.out.print("the max temp is "+M);
-    }*/
+    }
     
     /**question 27
     */
- /**   public void temperatureInfo(temperatures){
-        temperatures.sort();
+    public void temperatureInfo27(ArrayList<Double> temperatures){
+        Collections.sort(temperatures);
         double M;
-        M=temperatures(temperatures.size()-1); //index of the last value in the list
+        M=temperatures.get(temperatures.size()-1); //index of the last value in the list
         System.out.print("max temp: "+M);
-    }   */
+    }   
 
 }
